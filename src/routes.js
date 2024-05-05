79ea83448cea8895ba9f6a7361493da9f155ed2e
ChandraRaditya <chandraariwiraditya@gmail.com>
2024-05-05 17:18:36 +0700

    feat: add post api for note

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,15 @@
+const { addNoteHandler } = require('./handler');
+
 const routes = [
   {
     method: 'POST',
     path: '/notes',
-    handler: () => {},
+    handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: addNoteHandler,
   },
 ];
 
